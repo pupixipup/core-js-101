017ef7235e182e906d162a3f17363e7d584a818a
pupixipup <rkbitinas@gmail.com>
2022-07-20 20:14:15 +0200

    fix: eslint errors

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -283,8 +283,8 @@ function getSecondItems(arr) {
  *  [ 1,2,3,4,5 ] => [ 1, 2,2, 3,3,3, 4,4,4,4, 5,5,5,5,5 ]
  */
 function propagateItemsByPositionIndex(arr) {
+  return arr;
   // Array(index).fill(element));
-   
 }
 
 
